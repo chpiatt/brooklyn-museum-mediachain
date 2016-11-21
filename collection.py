7467eb76893277818a556cc649ebc9b4cce79d5d
Pyython <vogonpoetry06@gmail.com>
2016-11-21 12:00:41 -0500

    removed unnecessary comment from collection.py

--- collection.py
+++ collection.py
@@ -5,9 +5,6 @@ headers = {'api_key': config.API_KEY}
 
 def main():
     collections = []
-    # API limits responses to 35 items at a time.
-    # While loop fetches the next 35 items and
-    # adds each item to a new JSON array until dataset is exhausted.
     r = requests.get(url, headers=headers)
     if len(r.json()['data']) > 0:
         print "Fetching: {} records".format(len(r.json()['data']))
